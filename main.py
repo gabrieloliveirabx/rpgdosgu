daf87c25c3cdbb94e462724678f55dd30ae29623
gabriel <gabriel@MacBook-Air-de-gabriel.local>
2026-09-23 23:09:13 -0300

    Versao 1 finalizada

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ while True:
             vida = 120
             ataque = 25
             defesa = 20
+            qtde_pocao = 2
+            pocao = 30
+            qtde_moedas = 0
             classe = "Guerreiro"
             print(f"Voce escolheu Guerreiro!\n Vida: {vida}\n Ataque: {ataque}\n Defesa: {defesa}\n")
             break
@@ -34,6 +37,9 @@ while True:
             vida = 80
             ataque = 40
             defesa = 10
+            qtde_pocao = 2
+            pocao = 30
+            qtde_moedas = 0
             classe = "Mago"
             print(f"Voce escolheu Mago!\n Vida: {vida}\n Ataque: {ataque}\n Defesa: {defesa}\n")
             break
@@ -41,6 +47,9 @@ while True:
             vida = 100
             ataque = 30
             defesa = 15
+            qtde_pocao = 2
+            pocao = 30
+            qtde_moedas = 0
             classe = "Arqueiro"
             print(f"Voce escolheu Arqueiro!\n Vida: {vida}\n Ataque: {ataque}\n Defesa: {defesa}\n")
             break
@@ -51,17 +60,22 @@ while True:
 primeiro_boss = random.randint(1, 3)
 match primeiro_boss:
     case 1:
+        nome_boss = "Goblin"
         vida_boss = 60
         ataque_boss = 15
         print(f"Um Goblin apareceu!\n Vida do inimigo: {vida_boss}\n Dano por ataque do inimigo: {ataque_boss}")
     case 2:
+        nome_boss = "Lobo"
         vida_boss = 50
         ataque_boss = 20
         print(f"Um Lobo apareceu!\n Vida do inimigo: {vida_boss}\n Dano por ataque do inimigo: {ataque_boss}\n")
     case 3:
+        nome_boss = "Esqueleto"
         vida_boss = 80
         ataque_boss = 12
         print(f"Um Esqueleto apareceu!\n Vida do inimigo: {vida_boss}\n Dano por ataque do inimigo: {ataque_boss}")
+    case _:
+        print("Opcao invalida!\n")
 
 #Início do combate
 combate = '''
@@ -74,8 +88,6 @@ COMBATE!
 '''
 print(combate)
 
-qtde_pocao = 2
-pocao = 30
 vida_max = vida
 
 while True:
@@ -83,10 +95,18 @@ while True:
     chance_critica = random.randint(1, 10)
     match opcao:
         case 1:
-            vida_boss -= ataque
-            if vida_boss < 0:
+            if chance_critica == 1 or chance_critica == 2:
+                ataque = ataque * 2
+                vida_boss -= ataque
+                print(f"Voce conseguiu um ATAQUE CRITICO!\nVoce causou {ataque} de dano, vida restante do boss: {vida_boss}.\n")
+            else:
+                vida_boss -= ataque
+                print(f"Voce deu um dano de {ataque} no boss, vida restante do boss {vida_boss}\n")
+            if vida_boss > 0:
+                vida -= ataque_boss
+                print(f"{nome_boss} atacou!\nVoce recebeu {ataque_boss} de dano.\nSua vida: {vida}\nVida do {nome_boss}: {vida_boss}")
+            else:
                 vida_boss = 0
-            print(f"Voce deu um dano de {ataque} no boss, vida restante do boss {vida_boss}\n")
         case 2:
             ataque_boss_defendido = ataque_boss / 2
             vida -= ataque_boss
@@ -101,7 +121,54 @@ while True:
                 cura = 30
                 vida += cura
                 print(f"Foi curado: {cura}\nVida total: {vida}")
+        case _:
+            print("Opcao invalida!\n")
                 
     if vida <= 0 or vida_boss <= 0:
         print(f"Parabéns, voce derrotou o boss!\nVida restante: {vida}\n")
         break 
+
+portas = ''''
+Voce encontrou duas portas, escolha qual das duas voce irá entrar!
+
+1- Porta de madeira
+2- Porta de pedra
+
+'''
+print(portas)
+escolha_portas = int(input("Sua escolha: \n"))
+
+match escolha_portas:
+    case 1:
+        recompensa = random.randint(1, 2)
+        if recompensa == 1:
+            qtde_pocao += 1
+            print("Voce encontrou uma pocao!")
+        else:
+            print("A porta estava presa. Nada aconteceu.")
+    case 2:
+        recompensa = random.randint(1, 2)
+        if recompensa == 1:
+            qtde_moedas += 50
+            print("Voce encontrou 50 moedas!")
+        else:
+            vida -= 20
+            print("Uma armadilha foi ativada, voce perdeu 20 de vida.")
+    case _:
+        print("Opcao invalida!\n")
+
+finalgame = f'''
+========== STATUS FINAL ==========
+
+Classe: {classe}
+Vida: {vida}
+Poções restantes: {qtde_pocao}
+Inimigo derrotado: {nome_boss}
+Moedas encontradas: {qtde_moedas}
+
+==================================
+
+Você conseguiu escapar da masmorra!
+
+'''
+print(finalgame)
